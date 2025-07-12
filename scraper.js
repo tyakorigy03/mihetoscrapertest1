62ec309e53ac9551b86f9716fce34360e6f0d698
tyakorigy03 <tyakorigy03@gmail.com>
2025-07-12 16:43:12 +0200

    update scraper.js

--- scraper.js
+++ scraper.js
@@ -2,7 +2,9 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
 
   await page.goto('https://mihetofilms.web.app/');
